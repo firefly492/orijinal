cabb6dd7a2e1d35b41c66a491f1dee73db7464dd
firefly492 <nok.tsujimura@gmail.com>
2020-09-21 09:55:28 +0900

    top_controller追加/posts_destroy設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/index'
-  root to: "users#index"
-  resources :posts, only: [:index, :new, :create]
+  get 'top/index'
+  root to: "top#index"
+  resources :posts
 end
